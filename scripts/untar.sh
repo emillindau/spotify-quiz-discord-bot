4377008250869fa33efba73eff8d4f46b5c254a5
Emil Lindau <emil@attach.io>
2018-02-15 16:49:38 +0100

    cont

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,8 +4,8 @@ set -x
 export NODE_ENV=production
 
 cd /var/bot/ && \
+pm2 stop index && \
 tar zxvf package.tgz -C . && \
 mv build/package.json . && \
 npm install && \
-pm2 stop index && \
 pm2 start index
