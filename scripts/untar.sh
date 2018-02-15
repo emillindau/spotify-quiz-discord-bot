b05e2b4af20cca2a07c2ffc30cdad270c6f8d84d
Emil Lindau <emil@attach.io>
2018-02-15 23:48:34 +0100

    Hopefully it works now

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -3,9 +3,19 @@ set -x
 
 export NODE_ENV=production
 
+checkpm2() {
+    if hash pm2 2>/dev/null; then
+        echo pm2 exists
+    else
+        npm install pm2 -g
+    fi
+}
+
+checkpm2
+
 cd /var/bot/ && \
-pm2 stop index && \
+pm2 stop index || echo Already stopped && \
 tar zxvf package.tgz -C . && \
-mv build/package.json . && \
+mv build/* . && \
 npm install && \
-pm2 start index
+pm2 start index.js
