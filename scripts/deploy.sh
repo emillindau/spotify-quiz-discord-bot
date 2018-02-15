a4b8e8cc68263b8cd2007f35a5fec7013b2e6b6d
Emil Lindau <emil@attach.io>
2018-02-15 19:21:10 +0100

    Minor changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 set -x
 
+openssl aes-256-cbc -K $encrypted_14dfe625469c_key -iv $encrypted_14dfe625469c_iv -in deploy-key.enc -out deploy-key -d
+rm deploy-key.enc # Don't need it anymore
+chmod 600 deploy-key
+mv deploy-key ~/.ssh/id_rsa
+
 tar -czf package.tgz build && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
